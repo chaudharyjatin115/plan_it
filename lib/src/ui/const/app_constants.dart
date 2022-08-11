2ac6732bb297aaa1391aa438a507a6a05b7cb2fe
Jatin Chaudhary <chaudharyjatin115@protonmail.com>
2022-08-11 17:12:33 +0530

    adding themes

---
+++ lib/src/ui/const/app_constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/widgets.dart';
+
+final containerColor = Color(0xff031651);
--- lib/src/ui/themes/theme.dart
+++ lib/src/ui/themes/theme.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 
 class MyThemes {
   static final darkTheme = ThemeData(
+      appBarTheme: AppBarTheme(
+        color: Color(
+          0xff3450A1,
+        ),
+      ),
+    
       primaryColor: Color(0xffeb06ff),
-      colorScheme: ColorScheme.dark(),
+      colorScheme: ColorScheme.dark(primary: Color(0xffeb06ff)),
       useMaterial3: true,
       primarySwatch: Colors.purple,
       scaffoldBackgroundColor: Color(
-        0xff324DA4,
+        0xff3450A1,
       ));
   static final lightTheme = ThemeData(
       scaffoldBackgroundColor: Colors.white,
@@ -15,5 +21,3 @@ class MyThemes {
       primaryColor: Color(0xff3450a1),
       colorScheme: ColorScheme.light(primary: Color(0xff324da4)));
 }
-
-final appThemeDataDark = ThemeData(backgroundColor: Color(0xff344FA1));
