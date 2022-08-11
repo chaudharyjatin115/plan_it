e2dc7596dc635ba4cac8790630296011dcfd9a9d
Jatin Chaudhary <chaudharyjatin115@protonmail.com>
2022-08-11 17:12:02 +0530

    adding some required models

---
+++ lib/src/models/todo_model.dart
@@ -0,0 +1,22 @@
+class ToDo {
+  final String taskName;
+  final bool isDone;
+
+  ToDo({required this.taskName, required this.isDone});
+}
+
+final List<ToDo> listTodo = [
+  ToDo(taskName: "eat something", isDone: false),
+  ToDo(taskName: "code something", isDone: true),
+  ToDo(taskName: "write a blog", isDone: false),
+  ToDo(taskName: "go out ", isDone: false),
+  ToDo(taskName: "excercise", isDone: true),
+  ToDo(taskName: "excercise", isDone: true),
+  ToDo(taskName: "excercise", isDone: true),
+  ToDo(taskName: "excercise", isDone: true),
+  ToDo(taskName: "excercise", isDone: true),
+  ToDo(taskName: "excercise", isDone: false),
+  ToDo(taskName: "excercise", isDone: true),
+  ToDo(taskName: "excercise", isDone: true),
+  ToDo(taskName: "excercise", isDone: true),
+];
