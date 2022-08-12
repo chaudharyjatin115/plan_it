6e6e578991ab3c374b27e9754bf76f55510c8f55
Jatin Chaudhary <chaudharyjatin115@protonmail.com>
2022-08-12 18:25:44 +0530

    adding some custom widgets

--- lib/src/models/todo_model.dart
+++ lib/src/models/todo_model.dart
@@ -5,7 +5,36 @@ class ToDo {
   ToDo({required this.taskName, required this.isDone});
 }
 
+final todoCatList = [
+  ToDoCategory('Buisness', 20, listTodo),
+  ToDoCategory('Personal', 3, listTodoTwo)
+];
+
+class ToDoCategory {
+  final String name;
+  final int length;
+  final List<ToDo> toDoList;
+
+  ToDoCategory(this.name, this.length, this.toDoList);
+}
+// debug data
+
 final List<ToDo> listTodo = [
+  ToDo(taskName: "eat something", isDone: false),
+  ToDo(taskName: "code something", isDone: true),
+  ToDo(taskName: "write a blog", isDone: false),
+  ToDo(taskName: "go out ", isDone: false),
+  ToDo(taskName: "excercise", isDone: true),
+  ToDo(taskName: "excercise", isDone: true),
+  ToDo(taskName: "excercise", isDone: true),
+  ToDo(taskName: "excercise", isDone: true),
+  ToDo(taskName: "excercise", isDone: true),
+  ToDo(taskName: "excercise", isDone: false),
+  ToDo(taskName: "excercise", isDone: true),
+  ToDo(taskName: "excercise", isDone: true),
+  ToDo(taskName: "excercise", isDone: true),
+];
+final List<ToDo> listTodoTwo = [
   ToDo(taskName: "eat something", isDone: false),
   ToDo(taskName: "code something", isDone: true),
   ToDo(taskName: "write a blog", isDone: false),
--- lib/src/ui/screens/home_screen.dart
+++ lib/src/ui/screens/home_screen.dart
@@ -8,73 +8,129 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          elevation: 0,
-          title: Text('Hey Olivia whatsup'),
-          pinned: true,
-          floating: false,
-          expandedHeight: 100,
-          collapsedHeight: 100,
-          actions: [
-            Expanded(
-              child: Icon(Icons.menu),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width - 200,
-            ),
-            Expanded(child: Icon(Icons.search)),
-            Expanded(child: Icon(Icons.notifications)),
-          ],
-        ),
-        SliverToBoxAdapter(
-          child: Container(
-            height: 150,
-            width: 60,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: listTodo.length,
-              itemBuilder: ((context, index) {
-                return Container(
-                  height: 150,
-                  width: 50,
-                );
-              }),
-            ),
+        floatingActionButton: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(40),
+            color: Theme.of(context).primaryColor,
+          ),
+          child: IconButton(
+            iconSize: 40,
+            icon: Icon(Icons.add),
+            onPressed: (() {}),
           ),
         ),
-        SliverList(
-          delegate: SliverChildBuilderDelegate(((context, index) {
-            return Padding(
-              padding: const EdgeInsets.only(
-                  left: 40, right: 40, bottom: 10, top: 20),
-              child: Container(
-                height: 75,
-                width: 500,
-                decoration: BoxDecoration(
-                  color: containerColor,
-                  borderRadius: BorderRadius.circular(25.0),
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+              elevation: 0,
+              title: Text('Hey Olivia whatsup'),
+              pinned: true,
+              floating: false,
+              expandedHeight: 100,
+              collapsedHeight: 100,
+              actions: [
+                Expanded(
+                  child: Icon(Icons.menu),
                 ),
-                child: Row(children: [
-                  Checkbox(
-                      shape: RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(10.0))),
-                      value: listTodo[index].isDone,
-                      onChanged: ((value) {})),
-                  // IconButton(
-                  //     onPressed: () {},
-                  //     icon: IconButton(
-                  //       icon: Icon(Icons.circle),
-                  //       onPressed: () {},
-                  //     ))
-                ]),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width - 200,
+                ),
+                Expanded(child: Icon(Icons.search)),
+                Expanded(child: Icon(Icons.notifications)),
+              ],
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: Container(
+                    height: 70,
+                    width: 100,
+                    child: Text(
+                      'Hey Olivia Whatsup',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w500, fontSize: 30),
+                    )),
               ),
-            );
-          }), childCount: listTodo.length),
-        ),
-      ],
-    ));
+            ),
+            SliverToBoxAdapter(
+              child: Container(height: 30),
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                height: 120,
+                width: MediaQuery.of(context).size.width,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: todoCatList.length,
+                  itemBuilder: ((context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      child: Container(
+                        child: Column(
+                          children: [
+                            Text(
+                              '${todoCatList[index].toDoList.length} tasks',
+                              style: TextStyle(
+                                  color: Colors.white60,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Container(
+                              child: Text(todoCatList[index].name),
+                            )
+                          ],
+                        ),
+                        decoration: BoxDecoration(
+                            color: containerColor,
+                            borderRadius: BorderRadius.circular(30.0)),
+                        height: 120,
+                        width: 200,
+                      ),
+                    );
+                  }),
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Container(height: 50),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(((context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(
+                      left: 40, right: 40, bottom: 10, top: 10),
+                  child: Container(
+                    height: 75,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      color: containerColor,
+                      borderRadius: BorderRadius.circular(25.0),
+                    ),
+                    child: Row(
+                      children: [
+                        Checkbox(
+                            checkColor: containerColor,
+                            shape: RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(10.0))),
+                            value: listTodo[index].isDone,
+                            onChanged: ((value) {})),
+                        SizedBox(
+                          width: 80,
+                        ),
+                        Text(
+                          listTodo[index].taskName,
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w300),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }), childCount: listTodo.length),
+            ),
+          ],
+        ));
   }
 }
